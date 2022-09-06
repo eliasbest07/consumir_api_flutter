aa5b40f159be6d201e9ccafb38fffd55e0cb93c2
EliasMontilla <eliasbest634@gmail.com>
2022-09-06 12:36:30 -0500

    comienzo

---
+++ consumirapi_demo/android/app/src/main/java/com/eliasbest/consumirapi_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.eliasbest.consumirapi_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ consumirapi_demo/lib/screens/homescreens.dart
@@ -0,0 +1,84 @@
+import 'package:consumirapi_demo/model/dato.dart';
+import 'package:flutter/material.dart';
+
+import 'package:consumirapi_demo/server/remote_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Post>? post;
+  bool isLoaded = false;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    //fetch data drom API
+  }
+
+  getData() async {
+    post = await RemoteService().getPosts();
+    if (post != null) {
+      isLoaded = true;
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Consumir API'),
+        ),
+        body: Visibility(
+          visible: isLoaded,
+          replacement: const Center(child: CircularProgressIndicator()),
+          child: ListView.builder(
+            itemCount: post?.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            post![index].title,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            post![index].body ?? '',
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ));
+  }
+}
